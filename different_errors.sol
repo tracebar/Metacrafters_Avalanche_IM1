fbb519276bcb30cbeae9bad2624fc1e499d7b5ac
tracebar <166094171+tracebar@users.noreply.github.com>
2024-04-09 20:52:53 +0530

    Update different_errors.sol

--- different_errors.sol
+++ different_errors.sol
@@ -4,17 +4,17 @@ pragma solidity ^0.8.13;
 contract Error_handling{
     string public state = "Number is divisible by 3";
     function _require(uint256 _x) public view returns (string memory){
-        require(_x > 0,"Input must be divisible by 3");
+        require(_x %3 == 0,"Input must be divisible by 3");
         return state;
     }
     function _revert(uint256 _x) public view returns (string memory){
-        if(_x <= 0){
+        if(_x%3 != 0){
             revert("Input must be divisible by 3");
         }
         return state;
     }
     function _assert(uint256 _x) public view returns (string memory){
-        assert(_x > 0);
+        assert(_x %3 == 0);
         return state;
     }
 }
