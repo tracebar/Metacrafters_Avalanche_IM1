42a83abca846e13a7945348273443ff3c1432bc4
tracebar <166094171+tracebar@users.noreply.github.com>
2024-04-09 20:51:25 +0530

    Update different_errors.sol

--- different_errors.sol
+++ different_errors.sol
@@ -2,18 +2,18 @@
 pragma solidity ^0.8.13;
 
 contract Error_handling{
-    string public state = "Number is positive";
-    function _require(int256 _x) public view returns (string memory){
-        require(_x > 0,"Input must be positive number");
+    string public state = "Number is divisible by 3";
+    function _require(uint256 _x) public view returns (string memory){
+        require(_x > 0,"Input must be divisible by 3");
         return state;
     }
-    function _revert(int256 _x) public view returns (string memory){
+    function _revert(uint256 _x) public view returns (string memory){
         if(_x <= 0){
-            revert("Input must be positive number");
+            revert("Input must be divisible by 3");
         }
         return state;
     }
-    function _assert(int256 _x) public view returns (string memory){
+    function _assert(uint256 _x) public view returns (string memory){
         assert(_x > 0);
         return state;
     }
